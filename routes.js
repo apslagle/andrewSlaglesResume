3188d9b20cc00fd13622966e87e7b30f5b437e1f
apslagle <apslagle@gmail.com>
2017-03-20 14:14:31 -0500

    add skills

--- routes.js
+++ routes.js
@@ -1,7 +1,19 @@
+var skills = require('./resume/skills.js')
 module.exports = function(app, express, db, wk, email) {
 
-  app.post('/api/home', (req, res) => {
-    //
+  app.post('/api/', (req, res) => {
+    var text;
+    let intent = req.body.result.metadata.intentName;
+    if (intent === 'skillQuery') {
+      let hasSkill;
+    }
+    var response = {
+      "speech": text,
+      "displayText": text,
+      "data": {},
+      "contextOut": [],
+      "source": "accomplish.life"
+    };
   });
 
   app.get('/', (req, res) => {
