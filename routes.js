8f7745ee867ca5befbed468f314191a8eb818db3
apslagle <apslagle@gmail.com>
2017-03-20 14:26:01 -0500

    add skill handling

--- routes.js
+++ routes.js
@@ -2,10 +2,16 @@ var skills = require('./resume/skills.js')
 module.exports = function(app, express, db, wk, email) {
 
   app.post('/api/', (req, res) => {
-    var text;
+    var text = "I'm not hitting anything";
     let intent = req.body.result.metadata.intentName;
     if (intent === 'skillQuery') {
-      let hasSkill;
+      let skill = req.body.result.parameters.skill;
+      let hasSkill = skills.includes(skill);
+      if (hasSkill) {
+        text = `Yes! Andrew does have experience with ${skill}.`
+      } else {
+        text = `No. Andrew hasn't had the opportunity to use ${skill} yet.`
+      }
     }
     var response = {
       "speech": text,
