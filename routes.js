52a116ce33b1b1a07d3021dfe1bd983b7e943bb9
apslagle <apslagle@gmail.com>
2017-03-20 13:36:14 -0500

    add server test

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 module.exports = function(app, express, db, wk, email) {
 
-  app.post('/api/home', function(req, res) {
+  app.post('/api/home', (req, res) => {
     //
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Test successful');
   });
 
 };
